70ffa45538ce5bf369367c41f5080bea20cd3dbd
tobias bianchi <tobias.bianchi93@gmail.com>
2016-09-19 00:00:33 -0300

    Axis visibility

--- globalVariables.js
+++ globalVariables.js
@@ -5,3 +5,5 @@ var Z = 2;
 var XAxis = [1,0,0];
 var YAxis = [0,1,0];
 var ZAxis = [0,0,1];
+
+var VISIBLE_AXIS = true;
--- objects3D/Object3D.js
+++ objects3D/Object3D.js
@@ -1,6 +1,7 @@
 function Object3D() {
     this.children = [];
     this.mMatrix = mat4.create();
+    this.axisBuffers = {};
     mat4.identity(this.mMatrix);
 
     function NotImplementedError(message) {
@@ -11,7 +12,99 @@ function Object3D() {
     this.build = function () {
         for (var i = 0; i < this.children.length; i++) {
             this.children[i].build();
+            this.children[i].buildAxis();
         }
+    }
+
+    this.buildAxis = function () {
+        var xaxisVertixs = [
+            0, 0.01, 0.01,
+            0, -0.01, -0.01,
+            2, 0, 0
+        ]
+        var yaxisVertixs = [
+            0.01, 0, 0.01,
+            0, 2, 0,
+            -0.01, 0, -0.01
+        ]
+
+        var zaxisVertixs = [
+            0.01, 0.01, 0,
+            0, 0, 2,
+            -0.01, -0.01, 0
+        ]
+        var xColor = [
+            1,0,0,
+            1,0,0,
+            1,0,0
+        ]
+        var yColor = [
+            0,1,0,
+            0,1,0,
+            0,1,0
+        ]
+        var zColor = [
+            0,0,1,
+            0,0,1,
+            0,0,1
+        ];
+        
+        var indexBuffer = [0, 1, 2];
+        var buffers = {};
+
+        function initializeAxis(position_buffer, color_buffer, axis_name) {
+            var webgl_position_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, webgl_position_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position_buffer), gl.STATIC_DRAW);
+
+            var webgl_color_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, webgl_color_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color_buffer), gl.STATIC_DRAW);
+
+            // Notar que esta vez se usa ELEMENT_ARRAY_BUFFER en lugar de ARRAY_BUFFER.
+            // Notar tambi-n que se usa un array de enteros en lugar de floats.
+            var webgl_indexBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, webgl_indexBuffer);
+            gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexBuffer), gl.STATIC_DRAW);
+            buffers[axis_name] = {
+                index: webgl_indexBuffer,
+                color: webgl_color_buffer,
+                position: webgl_position_buffer
+            }
+        }
+        if (VISIBLE_AXIS == true){
+            initializeAxis(xaxisVertixs,xColor,'x');
+            initializeAxis(yaxisVertixs,yColor,'y');
+            initializeAxis(zaxisVertixs,zColor,'z');
+            this.axisBuffers = buffers;
+        }
+    
+    }
+
+    this.drawAxis = function () {
+        function draw(buffers){
+            var position = buffers.position;
+            var color = buffers.color;
+            var index = buffers.index;
+            var vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+            gl.enableVertexAttribArray(vertexPositionAttribute);
+            gl.bindBuffer(gl.ARRAY_BUFFER, position);
+            gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+            var vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+            gl.enableVertexAttribArray(vertexColorAttribute);
+            gl.bindBuffer(gl.ARRAY_BUFFER, color);
+            gl.vertexAttribPointer(vertexColorAttribute, 3, gl.FLOAT, false, 0, 0);
+
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index);
+
+           gl.drawElements(gl.TRIANGLE_STRIP, 3, gl.UNSIGNED_SHORT, 0);
+        }
+        for (key in this.axisBuffers){
+            draw(this.axisBuffers[key]);
+        }
+
+
     }
 
     this.addChild = function (child) {
@@ -23,39 +116,39 @@ function Object3D() {
     }
 
     this.draw = function (matrix) {
-        
+        var aux = mat4.create();
+        mat4.multiply(aux, matrix, this.mMatrix);
         for (var i = 0; i < this.children.length; i++) {
-            mat4.multiply(this.mMatrix, matrix, this.mMatrix);
-            this.children[i].draw(this.mMatrix);
+            this.children[i].draw(aux);
+            this.children[i].drawAxis(aux);
         }
-        mat4.identity(this.mMatrix);
     }
-    
+
     this.removeChild = function () {
         return this.children.pop();
     }
 
-    function gradToRad(grados){
-        return grados*Math.PI/180;
+    function gradToRad(grados) {
+        return grados * Math.PI / 180;
     }
     this.translate = function (array3D) {
         mat4.translate(this.mMatrix, this.mMatrix, array3D);
     }
 
-    this.rotate = function (angle,rotateAxis) {
+    this.rotate = function (angle, rotateAxis) {
         angle = gradToRad(angle);
-        mat4.rotate(this.mMatrix, this.mMatrix, angle,rotateAxis);
+        mat4.rotate(this.mMatrix, this.mMatrix, angle, rotateAxis);
     }
 
     this.scale = function (scale) {
-        mat4.scale(this.mMatrix, this.mMatrix,scale);
+        mat4.scale(this.mMatrix, this.mMatrix, scale);
     }
 
-    this.setMatrixUniforms = function(mMatrix) {
-        
+    this.setMatrixUniforms = function (mMatrix) {
+
         gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
         gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mMatrix);
-        
+
         var normalMatrix = mat3.create();
         mat3.identity(normalMatrix);
         mat3.normalFromMat4(normalMatrix, mvMatrix);
