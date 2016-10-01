65a7dd5fa745cd7f93c12e5ef1f7d335efd1c14a
tobias bianchi <tobias.bianchi93@gmail.com>
2016-10-01 13:23:34 -0300

    Definition types of curves

--- curvas/curvaBezierCubica.js
+++ curvas/curvaBezierCubica.js
@@ -1,12 +1,21 @@
-var Base0,Base1,Base2,Base3;
-var Base0der,Base1der,Base2der,Base3der;
+function CurvaBezier3(puntosDeControl) {
+    Curva.call(this, puntosDeControl);
 
-Base0=function(u) { return (1-u)*(1-u)*(1-u);}  // 1*(1-u) - u*(1-u) = 1-2u+u2  ,  (1-2u+u2) - u +2u2- u3 ,  1 - 3u +3u2 -u3
-Base1=function(u) { return 3*(1-u)*(1-u)*u; }
-Base2=function(u) { return 3*(1-u)*u*u;}
-Base3=function(u) { return u*u*u; }
-// bases derivadas
-Base0der=function(u) { return -3*u*u+6*u-3;} //-3u2 +6u -3
-Base1der=function(u) { return 9*u*u-12*u+3; }  // 9u2 -12u +3
-Base2der=function(u) { return -9*u*u+6*u;}		 // -9u2 +6u
-Base3der=function(u) { return 3*u*u; }			// 3u2
+    var Base0, Base1, Base2, Base3;
+    var Base0der, Base1der, Base2der, Base3der;
+
+    Base0 = function (u) { return (1 - u) * (1 - u) * (1 - u); }  // 1*(1-u) - u*(1-u) = 1-2u+u2  ,  (1-2u+u2) - u +2u2- u3 ,  1 - 3u +3u2 -u3
+    Base1 = function (u) { return 3 * (1 - u) * (1 - u) * u; }
+    Base2 = function (u) { return 3 * (1 - u) * u * u; }
+    Base3 = function (u) { return u * u * u; }
+    // bases derivadas
+    Base0der = function (u) { return -3 * u * u + 6 * u - 3; } //-3u2 +6u -3
+    Base1der = function (u) { return 9 * u * u - 12 * u + 3; }  // 9u2 -12u +3
+    Base2der = function (u) { return -9 * u * u + 6 * u; }		 // -9u2 +6u
+    Base3der = function (u) { return 3 * u * u; }			// 3u2    
+
+    this.basesFunctions = [Base0, Base1, Base2, Base3];
+    this.basesDerivatesFunctions = [Base0der, Base1der, Base2der, Base3der];
+
+}
+inheritPrototype(CurvaBezier3, Curva);
--- curvas/curvaBsplineCuadratica.js
+++ curvas/curvaBsplineCuadratica.js
@@ -1,10 +1,19 @@
-var Base0,Base1,Base2;
-var Base0der,Base1der,Base2der;
+function CurvaBspline2(puntosDeControl) {
+    Curva.call(this, puntosDeControl);
 
-Base0=function(u) { return 0.5*(1-u)*(1-u);}  
-Base1=function(u) { return 0.5*(1-u)*u; }
-Base2=function(u) { return 0.5*(u)*(u);}
-// bases derivadas
-Base0der=function(u) { return (u - 1);} 
-Base1der=function(u) { return 0.5-u; }
-Base2der=function(u) { return u;}
+    var Base0, Base1, Base2;
+    var Base0der, Base1der, Base2der;
+
+    Base0 = function (u) { return 0.5 * (1 - u) * (1 - u); }
+    Base1 = function (u) { return 0.5 * (1 - u) * u; }
+    Base2 = function (u) { return 0.5 * (u) * (u); }
+    // bases derivadas
+    Base0der = function (u) { return (u - 1); }
+    Base1der = function (u) { return 0.5 - u; }
+    Base2der = function (u) { return u; }
+
+    this.basesFunctions = [Base0,Base1,Base2];
+    this.basesDerivatesFunctions = [Base0der,Base1der,Base2der];
+
+}
+inheritPrototype(CurvaBspline2, Curva);
